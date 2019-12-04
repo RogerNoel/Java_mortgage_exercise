6440d2918a84bfff831bb0f7cefe65db61d7e4b7
RogerNoel <noel.roger.pro@gmail.com>
2019-12-04 16:48:07 +0100

    refact. user prompts in a readNumber function

--- src/eu/noelroger/Main.java
+++ src/eu/noelroger/Main.java
@@ -7,35 +7,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Variables
-        long principal = 0;
-        float annualRate = 0;
-        int numberOfPayments = 0;
         //----------
-        while (true) {
-            System.out.print("Entrez le montant à emprunter, entre 1000 et 1 million: ");
-            principal = scanner.nextLong();
-            if (principal >= 1000 && principal < 1000000)
-                break;
-            System.out.print("ATTENTION: le montant à emprunter doit se situer entre 1000 et 1 million: ");
-        }
-        do {
-            System.out.print("Entrez le taux ANNUEL (entre 0 et 13) en %: ");
-            annualRate = scanner.nextFloat();
-        } while (annualRate > 13);
-        while(true) {
-            System.out.print("Entrez le nombre de mensualités, entre 1 et 360 inclus: ");
-            numberOfPayments = scanner.nextInt();
-            if (numberOfPayments <= 360 && numberOfPayments > 0)
-                break;
-            System.out.print("ATTENTION: le nombre de mensualités doit se situer entre 1 et 60 inclus: ");
-        }
+        long principal = (long) readNumber("Entrez le montant à emprunter, entre 1000 et 1 million: ", 1000, 1_000_000);
+        float annualRate = (float) readNumber("Entrez le taux ANNUEL (entre 0 et 13) en %: ", 0, 13);
+        int numberOfPayments = (int) readNumber("Entrez le nombre de mensualités, entre 1 et 360 inclus: ", 1, 360);
         double refund = calculateRefund (principal, annualRate, numberOfPayments);
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
         String formattedRefund = formatter.format(refund);
         System.out.println("Le montant de la mensualité s'élèvera à " + formattedRefund);
         System.out.println("Le remboursement total s'élèvera à " + (formatter.format(numberOfPayments * refund)));
         scanner.close();
+    }
+
+    public static double readNumber(String prompt, Integer min, Integer max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.println(prompt);
+            value = scanner.nextDouble();
+                if (value >= min && value < max)
+                    break;
+            System.out.print("ATTENTION: la valeur doit se situer entre " + min + " et " + max);
+        }
+        scanner.close();
+        return value;
     }
 
     public static double calculateRefund (Long principal, Float annualRate, Integer numberOfPayments){
