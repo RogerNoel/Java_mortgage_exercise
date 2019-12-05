0696c346b4637fe09633ae802cce8e9bfedfebb6
RogerNoel <noel.roger.pro@gmail.com>
2019-12-05 12:48:32 +0100

    refact. calculateRefund()

--- src/eu/noelroger/Main.java
+++ src/eu/noelroger/Main.java
@@ -11,7 +11,6 @@ public class Main {
         long principal = 0;
         float annualRate = 0;
         int numberOfPayments = 0;
-        double refund = 0;
         //----------
         while (true) {
             System.out.print("Entrez le montant à emprunter, entre 1000 et 1 million: ");
@@ -31,21 +30,22 @@ public class Main {
                 break;
             System.out.print("ATTENTION: le nombre de mensualités doit se situer entre 1 et 60 inclus: ");
         }
+        calculateRefund(principal, annualRate, numberOfPayments);
+        calculateBalance(principal, annualRate, numberOfPayments);
+        scanner.close();
+    }
 
-        refund = calculateRefund(principal, annualRate, numberOfPayments);
+    public static double calculateRefund(Long principal, float annualRate, int  numberOfPayments) {
+        Scanner scanner = new Scanner(System.in);
+        double monthlyRate = annualRate/100/12;
+        double numerator = (Math.pow((1+monthlyRate), numberOfPayments))*monthlyRate;
+        double denominator = (Math.pow((1+monthlyRate), numberOfPayments))-1;
+        double refund = principal*(numerator/denominator);
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
         String formattedRefund = formatter.format(refund);
         System.out.println("Le montant de la mensualité s'élèvera à " + formattedRefund);
         System.out.println("Le remboursement total s'élèvera à " + (formatter.format(numberOfPayments * refund)));
         scanner.close();
-        calculateBalance(principal, annualRate, numberOfPayments);
-    }
-
-    public static double calculateRefund(Long principal, float annualRate, int  numberOfPayments) {
-        double monthlyRate = annualRate/100/12;
-        double numerator = (Math.pow((1+monthlyRate), numberOfPayments))*monthlyRate;
-        double denominator = (Math.pow((1+monthlyRate), numberOfPayments))-1;
-        double refund = principal*(numerator/denominator);
         return refund;
 
     }
@@ -53,13 +53,12 @@ public class Main {
         double monthlyRate = annualRate/100/12;
         int performedPayments = 0;
         while (performedPayments <= numberOfPayments ) {
-            System.out.println(performedPayments);
             double numerator = (Math.pow((1+monthlyRate), numberOfPayments))-(Math.pow((1+monthlyRate), performedPayments));
             double denominator = (Math.pow((1+monthlyRate), numberOfPayments))-1;
             double balance = principal*(numerator/denominator);
             NumberFormat formatter = NumberFormat.getCurrencyInstance();
             String formattedBalance = formatter.format(balance);
-            System.out.println(formattedBalance);
+            System.out.println("Après le paiement n° " + performedPayments + " le solde sera de " + formattedBalance);
             performedPayments++;
         }
     }
