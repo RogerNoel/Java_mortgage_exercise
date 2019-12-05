3e14dabd462fed912e5b4a0e594ac8d0b5be443c
RogerNoel <noel.roger.pro@gmail.com>
2019-12-05 12:39:17 +0100

    rev. to older commit + cre. calculateBalance()

--- src/eu/noelroger/Main.java
+++ src/eu/noelroger/Main.java
@@ -7,37 +7,60 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Variables
+        long principal = 0;
+        float annualRate = 0;
+        int numberOfPayments = 0;
+        double refund = 0;
         //----------
-        long principal = (long) readNumber("Entrez le montant à emprunter, entre 1000 et 1 million: ", 1000, 1_000_000);
-        float annualRate = (float) readNumber("Entrez le taux ANNUEL (entre 0 et 13) en %: ", 0, 13);
-        int numberOfPayments = (int) readNumber("Entrez le nombre de mensualités, entre 1 et 360 inclus: ", 1, 360);
-        double refund = calculateRefund (principal, annualRate, numberOfPayments);
+        while (true) {
+            System.out.print("Entrez le montant à emprunter, entre 1000 et 1 million: ");
+            principal = scanner.nextLong();
+            if (principal >= 1000 && principal < 1000000)
+                break;
+            System.out.print("ATTENTION: le montant à emprunter doit se situer entre 1000 et 1 million: ");
+        }
+        do {
+            System.out.print("Entrez le taux ANNUEL (entre 0 et 13) en %: ");
+            annualRate = scanner.nextFloat();
+        } while (annualRate > 13);
+        while(true) {
+            System.out.print("Entrez le nombre de mensualités, entre 1 et 60 inclus: ");
+            numberOfPayments = scanner.nextInt();
+            if (numberOfPayments <= 60 && numberOfPayments > 0)
+                break;
+            System.out.print("ATTENTION: le nombre de mensualités doit se situer entre 1 et 60 inclus: ");
+        }
+
+        refund = calculateRefund(principal, annualRate, numberOfPayments);
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
         String formattedRefund = formatter.format(refund);
         System.out.println("Le montant de la mensualité s'élèvera à " + formattedRefund);
         System.out.println("Le remboursement total s'élèvera à " + (formatter.format(numberOfPayments * refund)));
         scanner.close();
+        calculateBalance(principal, annualRate, numberOfPayments);
     }
 
-    public static double readNumber(String prompt, Integer min, Integer max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true) {
-            System.out.println(prompt);
-            value = scanner.nextDouble();
-                if (value >= min && value < max)
-                    break;
-            System.out.print("ATTENTION: la valeur doit se situer entre " + min + " et " + max);
-        }
-        scanner.close();
-        return value;
-    }
-
-    public static double calculateRefund (Long principal, Float annualRate, Integer numberOfPayments){
+    public static double calculateRefund(Long principal, float annualRate, int  numberOfPayments) {
         double monthlyRate = annualRate/100/12;
         double numerator = (Math.pow((1+monthlyRate), numberOfPayments))*monthlyRate;
         double denominator = (Math.pow((1+monthlyRate), numberOfPayments))-1;
         double refund = principal*(numerator/denominator);
         return refund;
+
+    }
+    public static void calculateBalance(Long principal, Float annualRate, Integer numberOfPayments ) {
+        double monthlyRate = annualRate/100/12;
+        int performedPayments = 0;
+        while (performedPayments <= numberOfPayments ) {
+            System.out.println(performedPayments);
+            double numerator = (Math.pow((1+monthlyRate), numberOfPayments))-(Math.pow((1+monthlyRate), performedPayments));
+            double denominator = (Math.pow((1+monthlyRate), numberOfPayments))-1;
+            double balance = principal*(numerator/denominator);
+            NumberFormat formatter = NumberFormat.getCurrencyInstance();
+            String formattedBalance = formatter.format(balance);
+            System.out.println(formattedBalance);
+            performedPayments++;
+        }
     }
 }
